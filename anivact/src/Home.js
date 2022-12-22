647afcb7ab5f0c978442ca6f0148e9ac1eaf5b01
Antonio Luis A. Martinez <luismgamedev@gmail.com>
2022-12-22 12:09:31 +0800

    feature: removed home

---
+++ anivact/src/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="App">
+      <div className="nav-bar">
+        <ol>
+          <li>Home</li>
+        </ol>
+      </div>
+      <h1>Welcome to Anivact!</h1>
+    </div>
+  );
+}
+
+export default Home;
