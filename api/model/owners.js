6acf9e481cd3f147de164aa9c1e017fbbc95f559
Antonio Luis A. Martinez <luismgamedev@gmail.com>
2023-01-07 20:29:11 +0800

    feature: model updated added fields

---
+++ api/model/owners.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, DataTypes, Model) => {
+    class Owners extends Model {}
+
+    Owners.init(
+        {
+            // Model attributes are defined here
+            username: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            pet: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            mobile_num: {
+                type: DataTypes.STRING,
+            },
+        },
+        {
+            sequelize, // We need to pass the connection instance
+            modelName: 'owners', // We need to choose the model name
+        }
+    )
+
+    return Owners
+}
