59a9b2a8206186d302646dc8c5edd0b060ede483
Antonio Luis A. Martinez <luismgamedev@gmail.com>
2023-01-08 17:16:07 +0800

    feature: added 3 other crud operations

--- api/controller/owners.js
+++ api/controller/owners.js
@@ -6,20 +6,22 @@ class OwnersController {
         logger.info('Controller: getOwners')
         return await userService.getOwners()
     }
+    // Assignment #1
+    // Uncomment remaining crud
 
-    // async createOwners(user) {
-    //     logger.info('Controller: createOwners', user)
-    //     return await userService.createOwners(user)
-    // }
+    async createOwners(user) {
+        logger.info('Controller: createOwners', user)
+        return await userService.createOwners(user)
+    }
 
-    // async updateOwners(user) {
-    //     logger.info('Controller: updateOwners', user)
-    //     return await userService.updateOwners(user)
-    // }
+    async updateOwners(user) {
+        logger.info('Controller: updateOwners', user)
+        return await userService.updateOwners(user)
+    }
 
-    // async deleteOwners(userId) {
-    //     logger.info('Controller: deleteOwners', userId)
-    //     return await userService.deleteOwners(userId)
-    // }
+    async deleteOwners(userId) {
+        logger.info('Controller: deleteOwners', userId)
+        return await userService.deleteOwners(userId)
+    }
 }
 module.exports = new OwnersController()
