5ee1fa20642fffd7709673e756131dc3b4a2d8f5
Antonio Luis A. Martinez <luismgamedev@gmail.com>
2023-01-09 19:06:53 +0800

    feature: added 3 api endpoints

--- api/service/owners.js
+++ api/service/owners.js
@@ -6,18 +6,20 @@ class OwnerService {
     async getOwners() {
         return await ownerRepository.getOwners()
     }
+    // Assignment #2
+    // Uncomment remaining crud
 
-    // async createOwner(owner) {
-    //     return await ownerRepository.createOwner(owner)
-    // }
+    async createOwner(owner) {
+        return await ownerRepository.createOwner(owner)
+    }
 
-    // async updateOwner(owner) {
-    //     return await ownerRepository.updateOwner(owner)
-    // }
+    async updateOwner(owner) {
+        return await ownerRepository.updateOwner(owner)
+    }
 
-    // async deleteOwner(ownerId) {
-    //     return await ownerRepository.deleteOwners(ownerId)
-    // }
+    async deleteOwner(ownerId) {
+        return await ownerRepository.deleteOwners(ownerId)
+    }
 }
 
 module.exports = new OwnerService()
