f9d14d347bc51939d12ee1bfadf2b322e022c39e
Antonio Luis A. Martinez <luismgamedev@gmail.com>
2023-01-30 22:00:06 +0800

    feature: updated styles on log in and dashboards

--- ui/src/components/authentication/Login.js
+++ ui/src/components/authentication/Login.js
@@ -31,35 +31,28 @@ function Login({ setIsAuthenticated }) {
             return
         } else {
             // props.login({ email, password })
-            console.log(
-                'this is the data in the frontend from log in hahaha ',
-                'this is the email hahahaha ',
-                email,
-                'password ito lets go',
-                password
-            )
             const credentials = { email, password }
             // const response =  LoginUser(credentials)
             // if (!response) {
             //     setError('ERRORS FOUND: ', response)
             // }
-            setIsAuthenticated(true)
-            return <Navigate to="/" />
+            // setIsAuthenticated(true)
+            // return <Navigate to="/" />
         }
     }
 
     return (
         <div className="w-full h-[75vh] flex flex-col justify-center items-center">
-            <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 content-center">
+            <form className="content-center px-8 pt-6 pb-8 mb-4 bg-white rounded shadow-md">
                 <div className="mb-4">
                     <label
-                        className="block text-gray-700 text-sm font-bold mb-2"
+                        className="block mb-2 text-sm font-bold text-gray-700"
                         // for="username"
                     >
                         Email Address
                     </label>
                     <input
-                        className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        className="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                         id="email"
                         type="email"
                         value={email}
@@ -69,26 +62,26 @@ function Login({ setIsAuthenticated }) {
                 </div>
                 <div className="mb-6">
                     <label
-                        className="block text-gray-700 text-sm font-bold mb-2"
+                        className="block mb-2 text-sm font-bold text-gray-700"
                         // for="password"
                     >
                         Password
                     </label>
                     <input
-                        className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                        className="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline"
                         id="password"
                         type="password"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         placeholder="******************"
                     />
-                    {/* <p className="text-red-500 text-xs italic">
+                    {/* <p className="text-xs italic text-red-500">
                         Please choose a password.
                     </p> */}
                 </div>
                 <div className="flex items-center justify-between">
                     <button
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                        className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline"
                         type="submit"
                         onClick={handleSubmit}
                     >
@@ -96,7 +89,7 @@ function Login({ setIsAuthenticated }) {
                     </button>
 
                     <a
-                        className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+                        className="inline-block text-sm font-bold text-blue-500 align-baseline hover:text-blue-800"
                         href="/signup"
                     >
                         Register now!
@@ -104,7 +97,7 @@ function Login({ setIsAuthenticated }) {
                 </div>
                 {error && (
                     <div
-                        className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 mt-5 rounded relative"
+                        className="relative px-4 py-3 mt-5 text-red-700 bg-red-100 border border-red-400 rounded"
                         role="alert"
                     >
                         <span className="block sm:inline">{error}</span>
--- ui/src/components/dashboards/doctors/CreateDoctor.js
+++ ui/src/components/dashboards/doctors/CreateDoctor.js
@@ -12,13 +12,18 @@ export default function CreateDoctor(props) {
     }
 
     return (
-        <div className="container">
+        <div className="container w-full max-w-lg ">
             <div className="row">
                 <div className="col-md-12 mrgnbtm">
                     <h2>Add Record</h2>
                     <form onSubmit={handleSubmit(onSubmit)}>
                         <div className="row mrgnbtm">
-                            <div className="form-group col-md-6">
+                            <div
+                                className="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-red-500 rounded appearance-none focus:outline-none focus:bg-white"
+                                id="grid-first-name"
+                                type="text"
+                                placeholder="Jane"
+                            >
                                 <label htmlFor="exampleInputUsername">
                                     Name
                                 </label>
